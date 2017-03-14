e6d927e61db0b75222aa1d8252b2b0e81d4ae680
akif121 <akif.hamid33@gmail.com>
2017-03-14 23:51:16 +0530

    Modified

--- App.java
+++ App.java
@@ -1,15 +1,10 @@
-import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 public class App {
-
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable(){
 			public void run() {
-				JFrame frame = new JFrame("Hello World");
-				frame.setSize(600,800);
-				frame.setVisible(true);
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+				new MainFrame();
 			}
 		});	
 	}
