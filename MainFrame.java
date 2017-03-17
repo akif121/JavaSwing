e50458082f22cb5e5ae3a865c0e624aa51b02da7
akif121 <akif.hamid33@gmail.com>
2017-03-18 02:09:12 +0530

    Added Listener

--- MainFrame.java
+++ MainFrame.java
@@ -6,22 +6,15 @@ import javax.swing.JFrame;
 
 public class MainFrame extends JFrame{
 		private TextPanel textPanel;
-		private JButton btn;
 		private Toolbar tb;
 		public MainFrame(){
 		super("Hello World");
 		setLayout(new BorderLayout());
 		textPanel = new TextPanel();
-		btn = new JButton("Click Me!");
 		tb = new Toolbar();
-		btn.addActionListener(new ActionListener(){
-		public void actionPerformed(ActionEvent arg0) {
-				textPanel.addText("Hello"+" World\n");
-		}
-		});
+		tb.setTextPanel(textPanel);
 		add(tb, BorderLayout.NORTH);
 		add(textPanel, BorderLayout.CENTER);
-		add(btn, BorderLayout.SOUTH);
 		setSize(500,500);
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
--- Toolbar.java
+++ Toolbar.java
@@ -1,24 +1,42 @@
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
 
-public class Toolbar extends JPanel {
+public class Toolbar extends JPanel implements ActionListener {
 	private JButton Hbtn;
 	private JButton Gbtn;
-	
+	private TextPanel textpanel;
 	public Toolbar(){
 		
 		Hbtn = new JButton("Hello");
 		Gbtn = new JButton("GoodBye");
 		
+		Hbtn.addActionListener(this);
+		Gbtn.addActionListener(this);
 		setLayout(new FlowLayout());
 		
 		add(Hbtn);
 		add(Gbtn);
 		
 	}
-	
+	public void setTextPanel(TextPanel textpanel){
+		this.textpanel = textpanel;
+	}
+	public void actionPerformed(ActionEvent e) {
+		JButton clicked = (JButton)e.getSource();
+		
+		if(clicked==Hbtn){
+			textpanel.addText("Hello\n");
+		}
+		else if (clicked==Gbtn)
+		{
+			textpanel.addText("Goodbye\n");
+		}
+		
+	}
 
 }
